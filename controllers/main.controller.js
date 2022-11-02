169693157c21da29607209bf693860742bf69a26
ProgramacionParapaquetes <programacion1@parapaquetes.com>
2022-11-01 18:10:52 -0600

    update controller main

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,5 +1,5 @@
 'use strict';
-const boxesSchema = require("../models/boxes");
+const boxeSchema = require("../models/boxes");
 const emailer = require("../config/emailer");
 
 class MainController {
@@ -8,7 +8,7 @@ class MainController {
     }
 
     getBox(_req, res) {
-        boxesSchema
+        boxeSchema
             .find()
             .then((data)=> res.json(data))
             .catch((error)=> res.json({message: `Error ${error}`}));
@@ -22,9 +22,9 @@ class MainController {
             .catch((error)=> res.json({message: `Error ${error}`}));
     }
 
-    createBox(req, res) {
+    async createBox (req, res) {
+        console.log(req.body)
         const { type_box, alto, largo, ancho } =  req.body;
-        console.log( type_box, alto, largo, ancho)
         let type_box_value;
         switch (type_box) {
             case '26_ects_C': 
@@ -44,7 +44,7 @@ class MainController {
             break;
         }
 
-        const boxes =  boxesSchema({
+        const boxes = boxeSchema({
             type_box, 
             type_box_value,
             alto, 
@@ -53,19 +53,75 @@ class MainController {
             precio_con_iva: priceWithIVA(type_box_value, parseFloat(alto), parseFloat(largo), parseFloat(ancho)),
             precio_sin_iva: priceWithoutIVA(type_box_value, parseFloat(alto), parseFloat(largo), parseFloat(ancho)),
             area: squareArea(parseFloat(alto), parseFloat(largo), parseFloat(ancho)),
-            peso_volumetrico: volumetricWeight(parseFloat(alto), parseFloat(largo), parseFloat(ancho))
+            peso_volumetrico: volumetricWeight(parseFloat(alto), parseFloat(largo), parseFloat(ancho)),
+            largo_pliego_cm: lengthTotalSheetCentimeters(parseFloat(largo), parseFloat(ancho)),
+            ancho_pliego_cm: widthTotalSheetCentimeters(parseFloat(ancho), parseFloat(alto)),
+            largo_pliego_mm: lengthTotalSheetMilimeters(parseFloat(largo), parseFloat(ancho)),
+            ancho_pliego_mm: widthTotalSheetMilimeters(parseFloat(ancho), parseFloat(alto)),
+
         })
+
+        // const data = await tracksModel.create(boxes)
+        // res.send(data)
         
         boxes
             .save()
             .then((data)=> {
                 emailer.sendMail(data)
-                res.render('calculator_boxes', { valores: data })
+                console.log(data)
+                res.send(data)
+            //    res.render('calculator_boxes', { valores: data })
             })
             .catch((error)=> res.json({message: `Error ${error}`}));
     }
+
+    
+
+    confirmQuote(req, res) {
+        res.render("confirmation_calculator_boxes");
+
+    }
 }
 
+function lengthTotalSheetCentimeters(largo, ancho) {
+    const largo_total_pliego = ((largo*2)+(ancho*2)+6)
+    return largo_total_pliego
+}
+
+function lengthTotalSheetMilimeters(largo, ancho) {
+    const largo1_extra = 5
+    const largo2_extra = 3
+    const largo_ceja = 35
+    const largo1_pliego = ((largo * 10) +  largo1_extra)
+    const largo2_pliego = ((ancho * 10) + largo2_extra)
+    const length_total_sheet_milimeters = ((largo1_pliego *2 ) + (largo2_pliego * 2)) + largo_ceja
+    const data_length_total_sheet_milimeters = {
+        largo1_pliego,
+        largo2_pliego,
+        largo1_extra,
+        largo2_extra,
+        largo_ceja,
+        length_total_sheet_milimeters
+    }
+    return data_length_total_sheet_milimeters
+}
+
+function widthTotalSheetMilimeters(ancho, alto) {
+    const ancho1_pliego = ((alto * 10) +  9)
+    const ancho2_pliego = ((ancho * 5) + 4)
+    const width_total_sheet_milimeters = ancho1_pliego + (ancho2_pliego * 2)
+    const data_length_total_sheet_milimeters = {
+        ancho1_pliego,
+        ancho2_pliego,
+        width_total_sheet_milimeters
+    }
+    return data_length_total_sheet_milimeters
+}
+
+function widthTotalSheetCentimeters(ancho, alto) {
+    const ancho_total_pliego = ancho * alto + 3
+    return ancho_total_pliego
+}
 
 function priceWithIVA(type_box_value, alto, largo, ancho) {
     var price_with_iva = ((((((largo * 10)* 2) + 10) + (((((ancho*10)*2)+6))+35)) * (((((alto*10)+9)+((alto*10)+8))/1000000) * type_box_value)) * 1.16);
