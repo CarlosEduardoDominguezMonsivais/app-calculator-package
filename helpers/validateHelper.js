6c7e556550a927b8350ca0a4d0c5bd1e1ec0f28b
ProgramacionParapaquetes <programacion1@parapaquetes.com>
2022-11-01 18:11:13 -0600

    update validateHelper

--- helpers/validateHelper.js
+++ helpers/validateHelper.js
@@ -1,14 +1,16 @@
 const { validationResult } = require('express-validator');
 
-const validateResult = (req, res, next) => {
+const validateResults = (req, res, next) => {
     try {
         validationResult(req).throw()
         return next()
     }catch(errors) {
         const valores = req.body
         const validaciones = errors.array()
-        res.render('calculator_boxes', {validaciones:validaciones, valores: valores})
+        res.status(403);
+        res.send({validaciones})
+        // res.render('calculator_boxes', {validaciones:validaciones, valores: valores})
     }
 }
 
-module.exports = { validateResult }
+module.exports =  validateResults 
