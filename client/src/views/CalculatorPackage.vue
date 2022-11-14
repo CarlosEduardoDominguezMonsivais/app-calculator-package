7f6f5224d78e3e65aa253381f22cc815748e0419
ProgramacionParapaquetes <programacion1@parapaquetes.com>
2022-11-14 10:50:32 -0600

    update the input padding of the measures

--- client/src/views/CalculatorPackage.vue
+++ client/src/views/CalculatorPackage.vue
@@ -109,10 +109,10 @@
                 </div>
               </div>
               <div class="form-group">
-                <div class="grid grid-cols-1 lg:grid-cols-3 gap-x-8 gap-y-6">
+                <div class="grid grid-cols-1 xl:grid-cols-3 gap-x-8 gap-y-6">
                   <div>
                     <div class="flex">
-                      <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                      <span class="inline-flex items-center px-3 text-xs text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         Largo
                       </span>
                       <input 
@@ -120,7 +120,7 @@
                           id="largo" 
                           class="rounded-none border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           v-model="quote.largo"/>
-                      <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-r-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                      <span class="inline-flex items-center px-3 text-xs text-gray-900 bg-gray-200 rounded-r-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         cm
                       </span>
                     </div>
@@ -128,7 +128,7 @@
                   </div>
                   <div>
                     <div class="flex">
-                      <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                      <span class="inline-flex items-center px-3 text-xs text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         Ancho
                       </span>
                       <input 
@@ -136,7 +136,7 @@
                           id="ancho" 
                           class="rounded-none border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           v-model="quote.ancho"/>
-                      <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-r-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                      <span class="inline-flex items-center px-3 text-xs text-gray-900 bg-gray-200 rounded-r-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         cm
                       </span>
                     </div>
@@ -144,7 +144,7 @@
                   </div>
                   <div>
                     <div class="flex">
-                      <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                      <span class="inline-flex items-center px-3 text-xs text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         Alto
                       </span>
                       <input 
@@ -152,7 +152,7 @@
                           id="alto"
                           class="rounded-none border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           v-model="quote.alto"/>    
-                      <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-r-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                      <span class="inline-flex items-center px-3 text-xs text-gray-900 bg-gray-200 rounded-r-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                         cm
                       </span>                   
                     </div>
