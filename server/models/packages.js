b04b7296ec744a6fcb1978c6d24bd8ea228342ca
ProgramacionParapaquetes <programacion1@parapaquetes.com>
2022-11-25 17:16:42 -0600

    update company name restriction

--- server/models/packages.js
+++ server/models/packages.js
@@ -29,7 +29,7 @@ const packageSchema = mongoose.Schema(
             },
             company: {
                 type: String,
-                required: true
+                required: false
             },
             email: {
                 type: String,
