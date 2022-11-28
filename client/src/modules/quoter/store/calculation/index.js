b3d695c71bd6b3bf8c363880485f321d7abed6e2
ProgramacionParapaquetes <programacion1@parapaquetes.com>
2022-11-28 11:38:26 -0600

    delete sessionStorage in the store

--- client/src/modules/quoter/store/calculation/index.js
+++ client/src/modules/quoter/store/calculation/index.js
@@ -1,17 +1,10 @@
 import state from './state'
 import * as mutations from './mutations'
-import VuexPersistence from 'vuex-persist'
 
 const calculationStore = {
     namespaced: true,
     state,
-    mutations,
-    plugins: [
-        new VuexPersistence({
-            key: 'vuex',
-            storage: window.sessionStorage
-        }).plugin
-    ]
+    mutations
 }
 
 export default calculationStore
